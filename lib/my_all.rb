4428c3078219db80acddded40402de9a4e0fb6f5
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-10 20:37:29 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,7 +3,8 @@ require 'pry'
 def my_all?(collection)
   i = 0 
   while i < collection.length
-  block_return_values << yield(collection[i])
+  block_return_values 
+  << yield(collection[i])
     i = i += 1 
   end
 end
